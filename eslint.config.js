a31aee830ccb8584ffd8662095885ca10ad96393
Anna St. Clair <ars82@calvin.edu>
2025-12-03 16:17:25 -0500

    fixed config error

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,45 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const js = require('@eslint/js');
+const globals = require('globals');
+const tseslint = require('typescript-eslint');
+const pluginReact = require('eslint-plugin-react');
 
-module.exports = defineConfig([
-  expoConfig,
+module.exports = [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     settings: {
+      react: {
+        version: 'detect',
+      },
       'import/resolver': {
         typescript: {
           project: './tsconfig.json',
         },
       },
     },
-    ignores: ['dist/*'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-]);
+  {
+    files: ['eslint.config.js', 'scripts/**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
